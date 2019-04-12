0324ea2e5232458f163e004561584419e2cd8120
Magesh Chandramouli <mchandramouli@expedia.com>
2019-04-12 10:46:39 +0100

    adding unit tests

--- s3-store/src/main/java/com/expedia/blobs/stores/aws/UploadProgressListener.java
+++ s3-store/src/main/java/com/expedia/blobs/stores/aws/UploadProgressListener.java
@@ -22,27 +22,29 @@ import com.amazonaws.event.ProgressListener;
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 
-public class UploadProgressListener implements ProgressListener {
+class UploadProgressListener implements ProgressListener {
     private final Logger logger;
     private final String key;
 
-    UploadProgressListener(Logger logger, String fileKey) {
+    UploadProgressListener(Logger logger, String objectKey) {
         Validate.notNull(logger);
         this.logger = logger;
 
-        Validate.notEmpty(fileKey);
-        this.key = fileKey;
+        Validate.notEmpty(objectKey);
+        this.key = objectKey;
     }
 
     @Override
     public void progressChanged(ProgressEvent progressEvent) {
+        final String msg = String.format("Progress event=%s file=%s transferred=%d", progressEvent.getEventType(), key,
+                                         progressEvent.getBytesTransferred());
         switch (progressEvent.getEventType()) {
             case TRANSFER_FAILED_EVENT:
             case TRANSFER_PART_FAILED_EVENT:
-                logger.error("Progress event failed for key {} : {} => {}", key, progressEvent.getEventType(), progressEvent.getBytesTransferred());
+                logger.error(msg);
                 break;
             default:
-                logger.info("Progress event for key {} : {} => {}", key, progressEvent.getEventType(), progressEvent.getBytesTransferred());
+                logger.info(msg);
                 break;
         }
     }
