529f17890d9b12654d7433f7645a9d027c2d03a8
Magesh Chandramouli <mchandramouli@expedia.com>
2019-04-19 12:52:24 +0530

    starting unit tests for blob store

---
+++ s3-store/src/test/scala/com/expedia/blobs/stores/aws/S3BlobStoreSpec.scala
@@ -0,0 +1,12 @@
+package com.expedia.blobs.stores.aws
+
+import org.scalatest.{BeforeAndAfter, FunSpec, GivenWhenThen, Matchers}
+import org.scalatest.easymock.EasyMockSugar._
+import org.easymock.EasyMock.{replay, verify}
+import scala.collection.JavaConverters._
+
+class S3BlobStroreSpec extends FunSpec with GivenWhenThen with BeforeAndAfter with Matchers {
+  describe("a class that acts as a blob store backed by s3") {
+    it("should ")
+  }
+}
