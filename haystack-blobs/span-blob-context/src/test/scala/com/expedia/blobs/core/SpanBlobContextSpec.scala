419a41da85c9561f018f27850e392f95d5ab40c6
Ashish <ashish@tilth.co>
2019-09-09 16:12:20 +0530

    blob-makekey-updates

--- haystack-blobs/span-blob-context/src/test/scala/com/expedia/blobs/core/SpanBlobContextSpec.scala
+++ haystack-blobs/span-blob-context/src/test/scala/com/expedia/blobs/core/SpanBlobContextSpec.scala
@@ -1,35 +1,18 @@
 package com.expedia.blobs.core
 
-import com.expedia.www.haystack.client.Tracer
-import com.expedia.www.haystack.client.dispatchers.NoopDispatcher
-import com.expedia.www.haystack.client.metrics.NoopMetricsRegistry
-import org.junit.Assert
 import org.scalatest.easymock.EasyMockSugar
 import org.scalatest.{FunSpec, Matchers}
 
 class SpanBlobContextSpec extends FunSpec with Matchers with EasyMockSugar {
 
-  private val tracer = new Tracer.Builder(new NoopMetricsRegistry, "TestService", new NoopDispatcher).build()
-  private val span = tracer.buildSpan("TestOperation").start
-
   describe("com.expedia.blobs.core.SpanBlobContext") {
 
     it("should throw an error if span is not present") {
       val catchExpection = intercept[Exception] {
-        val spanBlobContext: SpanBlobContext = new SpanBlobContext(null)
+        val _ = new SpanBlobContext(null, "", "")
       }
 
-      catchExpection.getMessage shouldEqual "Span cannot be null in context"
-    }
-
-    it("should return the correct operation name") {
-      val operationName = new SpanBlobContext(span).getOperationName
-      Assert.assertEquals("TestOperation", operationName)
-    }
-
-    it("should return the correct service name") {
-      val serviceName = new SpanBlobContext(span).getServiceName
-      Assert.assertEquals("TestService", serviceName)
+      catchExpection.getMessage shouldEqual "span cannot be null in context"
     }
   }
 }
